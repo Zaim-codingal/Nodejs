eb39e94f1c36676a0205589d7d963c38cafb94d1
Zaim-codingal <okay.contentfr@gmail.com>
2026-03-15 12:47:14 +0500

    commit 1

--- server6.js
+++ server6.js
@@ -1,18 +1,33 @@
-var SQL = require('mysql')
+var my_sql = require("mysql");
 
-var con = mysql.createConnection({
-    host: "",
-    user: "",
-    password: "",
+var con = my_sql.createConnection({
+
+host: "sql.freedb.tech",
+
+user: "freedb_studentABC",
+
+password: "BtVwj93Ck!beyu",
+
+database: "freedb_studentABC",
+
+port: 3306
+
 });
 
 con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    varsqlwork = "CREATE DATABASE student(StudentID int, StudentName varchar(255), StudentCity varchar(255))";
-    con.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log("Database created");
-    });
+
+if (err) throw err;
+
+console.log("Connected!");
+
+var sql = "CREATE TABLE student (StudentID INT, StudentName VARCHAR(255), StudentCity VARCHAR(255))";
+
+con.query(sql, function(err, result) {
+
+if (err) throw err;
+
+console.log("Table created");
+
 });
 
+});
