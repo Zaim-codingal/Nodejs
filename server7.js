e5f6122e39d329b95bde645bdf347795b64c7782
Zaim-codingal <okay.contentfr@gmail.com>
2026-03-15 12:47:36 +0500

    commit 1

---
+++ server7.js
@@ -0,0 +1,38 @@
+var my_sql = require("mysql");
+
+var con = my_sql.createConnection({
+
+host: "sql.freedb.tech",
+
+user: "freedb_studentABC",
+
+password: "BtVwj93Ck!beyu",
+
+database: "freedb_studentABC",
+
+port: 3306
+
+});
+
+con.connect(function(err) {
+
+if (err) throw err;
+
+console.log("Connected!");
+
+var sql = "CREATE TABLE student (StudentID INT, StudentName VARCHAR(255), StudentCity VARCHAR(255))";
+
+con.query(sql, function(err, result) {
+
+if (err) throw err;
+console.log("Table created");
+
+var sql = "ALTER TABLE student ADD Student_Grade INT";
+con.query(sql, function(err, result) {
+if (err) throw err;
+console.log("Table Altered");
+});
+
+});
+
+});
---
+++ server9.js
@@ -0,0 +1,29 @@
+var my_sql = require("mysql");
+
+var con = my_sql.createConnection({
+
+host: "sql.freedb.tech",
+
+user: "freedb_studentABC",
+
+password: "BtVwj93Ck!beyu",
+
+database: "freedb_studentABC",
+
+port: 3306
+
+});
+
+con.connect(function(err) {
+
+if (err) throw err;
+
+console.log("Connected!");
+
+var sql = " INSERT INTO student (StudentID, StudentFirstName, StudentLastName, StudentCity, StudentGrade) Values (05, 'Stu', 'Dent', 'CityTown', 67)";
+con.query(sql, function(err, result) {
+
+if (err) throw err;
+console.log("1 record inserted");
+});
+});
